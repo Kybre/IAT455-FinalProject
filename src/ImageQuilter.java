77fbaa697204cf9e78d384ce9825d9253b162b92
Kybre <hermanchan10@gmail.com>
2023-11-17 01:57:47 -0800

    added basic UI functionality to ImageQuilter.java

--- src/ImageQuilter.java
+++ src/ImageQuilter.java
@@ -1,18 +1,30 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-class ImageQuilter extends Frame {
+class ImageQuilter extends JFrame implements ActionListener {
 
 	BufferedImage srcImage, patternImage, finalImage;
-	int width, height;
+	double screenWidth, screenHeight;
+	int srcWidth, srcHeight, patternWidth, patternHeight;
+	Dimension screenSize;
+	JButton srcButton, patternButton;
 	
 	public ImageQuilter() {
 		//load images
@@ -24,42 +36,91 @@ class ImageQuilter extends Frame {
 			System.out.println("Cannot load the provided image");
 		}
 		this.setTitle("Image Quilter");
-		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		width = srcImage.getWidth();
-		height = srcImage.getHeight();
+		//buttons
+		srcButton = new JButton("Upload Source");
+		patternButton = new JButton("Upload Pattern");
 		
-		//Anonymous inner-class listener to terminate program
-		this.addWindowListener(
-			new WindowAdapter(){//anonymous class definition
-				public void windowClosing(WindowEvent e){
-					System.exit(0);//terminate the program
-				}//end windowClosing()
-			}//end WindowAdapter
-		);//end addWindowListener
+		srcButton.addActionListener(this);
+		patternButton.addActionListener(this);
+		
+		this.setLayout(null);
+		
+		this.add(srcButton);
+		this.add(patternButton);
+		
+		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setSize(screenSize);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == srcButton) {
+			File srcFile = uploadFile();
+			if(srcFile != null) {
+				try {
+					srcImage = ImageIO.read(srcFile);
+				} catch (Exception ex) {
+					System.out.println("Cannot load the provided image");
+				}
+			}
+		}
+		if(e.getSource() == patternButton) {
+			File patternFile = uploadFile();
+			if(patternFile != null) {
+				try {
+					srcImage = ImageIO.read(patternFile);
+				} catch (Exception ex) {
+					System.out.println("Cannot load the provided image");
+				}
+			}
+		}
+		repaint();
+	}
+	
+	public File uploadFile() {
+		JFileChooser jfc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "jpeg", "png");
+		
+		int returnVal = jfc.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+        	return jfc.getSelectedFile();
+        }
+        else return null;
 	}
 	
 	public void paint(Graphics g) {
+		super.paint(g);
 		
-		//if working with different images, this may need to be adjusted
-		int w = width/2; 
-		int h = height/2;
-
-		this.setSize(w * 2 + 300, h * 2 + 150);
-
-		g.drawImage(srcImage,25,50,w, h,this);
-	    g.drawImage(patternImage, 25+w+45, 50, w, h,this);
+		srcButton.setBounds(25, 25, 125, 25);
+		patternButton.setBounds(175, 25, 125, 25);
+		
+		g.setColor(new Color(20,20,20));
+		g.drawLine(25, 100, (int)screenSize.getWidth()-25, 100);
+		
+		//images
+		int sw = srcImage.getWidth();
+		int sh = srcImage.getHeight();
+		
+		int pw = patternImage.getWidth();
+		int ph = patternImage.getHeight();
+		
+		g.drawImage(srcImage, 25, 140, sw, sh, this);
+	    g.drawImage(patternImage, sw+75, 140, pw, ph, this);
+	    g.drawImage(srcImage, sw+pw+125, 140, sw, sh, this); //replace with final image
 
 	    g.setColor(Color.BLACK);
 	    Font f1 = new Font("Verdana", Font.PLAIN, 13); 
 	    g.setFont(f1); 
-	    g.drawString("Original Image", 25, 45); 
-	    g.drawString("Pattern Image",25+w+45, 45); 
+	    g.drawString("Original Image", 25, 125); 
+	    g.drawString("Pattern", sw+75, 125); 
+	    g.drawString("Result", sw+pw+125, 125); 
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ImageQuilter iq = new ImageQuilter();
+		iq.setVisible(true);
 		iq.repaint();
 	}
 
